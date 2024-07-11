7856631d235301a3e4b04c26ad9c8b491e7950e7
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-07-11 14:51:49 +0800

    added a website mail and its file content

---
+++ project/app/Mail/WebsiteMail.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class WebsiteMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $subject, $body;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($subject, $body)
+    {
+        $this->subject = $subject;
+        $this->body = $body;
+    }
+
+    public function build()
+    {
+        return $this->view('email.email')->with([
+            'subject' => $this->subject
+        ]);
+    }
+}
