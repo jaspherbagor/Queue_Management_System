71096992a0dc3ad1c26ce488ec8a6589e4ebec11
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-07-17 19:30:44 +0800

    removed the commented icons in the served card

--- project/resources/views/backend/admin/home/home.blade.php
+++ project/resources/views/backend/admin/home/home.blade.php
@@ -52,7 +52,6 @@
                 <div class="col-md-4 col-sm-6 col-xs-12 mb-1">
                     <div class="p-1 complete-token-card">
                         <img src="{{ asset('public/assets/img/sf/sign-check.svg') }} " alt="" class="icon-img">
-                        {{-- <i class="fa fa-check-circle-o fa-2x card_icon"></i> --}}
                         <h5><span class="card-qty">{{ (isset($infobox->token->complete)?$infobox->token->complete:0) }} Served</span></h5>
                     </div>
                 </div>
