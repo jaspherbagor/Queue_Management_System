7dbffb2823482bf694372531cf30cc5b1218fb31
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-07-17 19:29:18 +0800

    removed the commented icon in the user card

--- project/resources/views/backend/admin/home/home.blade.php
+++ project/resources/views/backend/admin/home/home.blade.php
@@ -31,7 +31,6 @@
                     <div class="col-md-4 col-sm-6 col-xs-12 mb-1">
                         <div class="p-1 user-card">
                             <img src="{{ asset('public/assets/img/sf/profile-group.svg') }} " alt="" class="icon-img">
-                            {{-- <i class="fa fa-users fa-2x card_icon"></i> --}}
                             <h5><span class="card-qty">{{ (isset($infobox->user)?$infobox->user:0) }} Users</span></h5>
                         </div>
                     </div>
