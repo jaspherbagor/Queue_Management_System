c29815d49360f46060a0b2cd62cc926779a41458
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-07-25 17:11:02 +0800

    updated the name of the counter to window in the modal

--- project/resources/views/backend/admin/counter/list.blade.php
+++ project/resources/views/backend/admin/counter/list.blade.php
@@ -68,8 +68,8 @@
         <h4 class="modal-title" id="infoModalLabel"><?= trans('app.note') ?></h4>
       </div>
       <div class="modal-body">
-        <p><strong class="label label-warning info-button"> Note 1 </strong> &nbsp;If you delete a Counter then, the related tokens are not calling on the Display screen. Because the token is dependent on Counter ID</p>
-        <p><strong class="label label-warning info-button"> Note 2 </strong> &nbsp;If you want to change a Counter name you must rename the Counter instead of deleting it.
+        <p><strong class="label label-warning info-button"> Note 1 </strong> &nbsp;If you delete a Window then, the related tokens are not calling on the Display screen. Because the token is dependent on Window ID</p>
+        <p><strong class="label label-warning info-button"> Note 2 </strong> &nbsp;If you want to change a Window name you must rename the Window instead of deleting it.
         </p>
       </div>
       <div class="modal-footer">
