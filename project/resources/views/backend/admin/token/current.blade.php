0c7682b1c08ce1b177bd33044f1c68771c5287a3
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-08-27 20:15:15 +0800

    updated the confirmation message on the current number page

--- project/resources/views/backend/admin/token/current.blade.php
+++ project/resources/views/backend/admin/token/current.blade.php
@@ -64,11 +64,11 @@
                                     <a href="{{ url("admin/token/complete/$token->id") }}"  class="btn btn-success btn-sm btn-complete mb-1" onclick="return confirm('Are you sure?')" title="Complete"><i class="fa fa-check"></i></a>
                                     <button type="button" data-toggle="modal" data-target=".transferModal" data-token-id='{{ $token->id }}' class="btn btn-primary btn-sm btn-transfer" title="Transfer"><i class="fa fa-exchange"></i></button> 
 
-                                    <a href="{{ url("admin/token/stoped/$token->id") }}"  class="btn btn-warning btn-sm btn-stop mb-1" onclick="return confirm('Are you sure?')" title="Stoped"><i class="fa fa-stop"></i></a>
+                                    <a href="{{ url("admin/token/stoped/$token->id") }}"  class="btn btn-warning btn-sm btn-stop mb-1" onclick="return confirm('Are you sure you want this number to stop?')" title="Stoped"><i class="fa fa-stop"></i></a>
 
                                     <button type="button" href='{{ url("admin/token/print") }}' data-token-id='{{ $token->id }}' class="tokenPrint btn btn-default btn-sm btn-print mb-1" title="Print" ><i class="fa fa-print"></i></button>
 
-                                    <a href='{{ url("admin/token/delete/$token->id") }}'class="btn btn-danger btn-sm btn-delete mb-1" onclick="return confirm('Are you sure?');" title="Delete"><i class="fa fa-trash"></i></a>
+                                    <a href='{{ url("admin/token/delete/$token->id") }}'class="btn btn-danger btn-sm btn-delete mb-1" onclick="return confirm('Are you sure you want to delete?');" title="Delete"><i class="fa fa-trash"></i></a>
                                 {{-- </div> --}}
                             </td>
                         </tr> 
