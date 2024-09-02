938787b2ed83e2b79beef52a25bbd7a6d31daa5e
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-09-02 21:00:22 +0800

    added a margin and padding to the body

--- project/resources/views/backend/admin/token/auto.blade.php
+++ project/resources/views/backend/admin/token/auto.blade.php
@@ -291,12 +291,12 @@
                     var formattedDate = formatDate(data.token.created_at);
 
                     var content = "<style type=\"text/css\">@media print {"+
-                    "html, body {margin:0; padding:0; overflow:hidden; display:block; width:100%; font-family:Arial, sans-serif;}"+
+                    "html, body {margin:10px; padding:5px; overflow:hidden; display:block; width:100%; font-family:Arial, sans-serif;}"+
                     ".receipt-token {width:100%; text-align:center; margin-top:30px;}"+
                     ".receipt-token h4 {margin:5px 0; padding:0; font-size:20px; line-height:24px;}"+
                     ".receipt-token h1 {margin:10px 0; padding:0; font-size:40px; line-height:45px;}"+
                     ".receipt-token ul {margin:0; padding:0; font-size:18px; line-height:22px; list-style:none; text-align:center; align-items:center; justify-content:center;}"+
-                    ".receipt-token ul li.date {margin-bottom:20px;}" +
+                    ".receipt-token ul li.date {margin-bottom:50px !important;}" +
                     ".receipt-token ul li {margin:5px 0;}"+
                     "}</style>";
 
