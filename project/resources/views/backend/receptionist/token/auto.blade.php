ca20f360826131cc24cc0726e30bcdd9fe2758d5
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-09-03 19:54:47 +0800

    updated the print functionality on the receptionist account

--- project/resources/views/backend/receptionist/token/auto.blade.php
+++ project/resources/views/backend/receptionist/token/auto.blade.php
@@ -254,30 +254,34 @@ $(document).ready(function(){
                 {
                     // Function to format the date
                     function formatDate(dateString) {
-                        var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true };
                         var date = new Date(dateString);
-                        return date.toLocaleDateString('en-US', options);
+                        var month = ('0' + (date.getMonth() + 1)).slice(-2); // Adding 1 because getMonth() returns 0-11
+                        var day = ('0' + date.getDate()).slice(-2);
+                        var year = date.getFullYear();
+
+                        return month + '/' + day + '/' + year;
                     }
 
                     // Example usage
                     var formattedDate = formatDate(data.token.created_at);
 
                     var content = "<style type=\"text/css\">@media print {"+
-                        "html, body {margin:0; padding:0; overflow:hidden; display:block; width:80mm; height:80mm; font-family:Arial, sans-serif; border:1px dotted #000;}"+
-                        ".receipt-token {width:100%; text-align:center; margin-top:5mm;}"+
-                        ".receipt-token h4 {margin:2mm 0; padding:0; font-size:14px; line-height:16px;}"+
-                        ".receipt-token h1 {margin:4mm 0; padding:0; font-size:24px; line-height:28px;}"+
-                        ".receipt-token ul {margin:0; padding:0; font-size:12px; line-height:14px; list-style:none; text-align:center; align-items:center; justify-content:center;}"+
-                        ".receipt-token ul li {margin:3mm 0;}"+
-                        "}</style>";
+                    "html, body {margin:0; padding:0; overflow:hidden; display:block; width:100%; font-family:Arial, sans-serif;}"+
+                    ".receipt-token {width:100%; text-align:center; margin-top:30px; margin-bottom:30px; border: 1px dotted black}"+
+                    ".receipt-token h4 {margin:5px 0; padding:0; font-size:20px; line-height:24px;}"+
+                    ".receipt-token h1 {margin:5px 0; padding:0; font-size:40px; line-height:30px;}"+
+                    ".receipt-token ul {margin:0; padding:0; font-size:12px; line-height:8px; list-style:none; text-align:center; align-items:center; justify-content:center;}"+
+                    ".receipt-token ul li.date {margin-bottom:20px !important;}" +
+                    ".receipt-token ul li {margin:5px 0;}"+
+                    "}</style>";
 
                     content += "<div class=\"receipt-token\">";
-                    content += "<h4>QUEUEING SYSTEM FOR PCLU</h4>";
+                    // content += "<h4>{{ \Session::get('app.title') }}</h4>";
                     content += "<h1>"+data.token.token_no+"</h1>";
                     content +="<ul>";
-                    content += "<li><strong>Department:</strong> "+data.token.department+"</li>";
                     content += "<li><strong>Window:</strong> "+data.token.counter+"</li>";
-                    content += "<li><strong>Date:</strong> "+formattedDate+"</li>";
+                    content += "<li><strong>Dept:</strong> "+data.token.department+"</li>";
+                    content += "<li class=\"date\"><strong>{{ trans('app.date') }}:</strong> "+formattedDate+"</li>";
                     content += "</ul>";
                     content += "</div>";
 
