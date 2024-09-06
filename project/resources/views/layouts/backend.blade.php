6b84ed75bd391c645454915ca8f35b314527a307
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-09-06 19:41:37 +0800

    added a list for the ads in the sidebar

--- project/resources/views/layouts/backend.blade.php
+++ project/resources/views/layouts/backend.blade.php
@@ -287,6 +287,9 @@
                                     <li class="{{ (Request::is('admin/setting/display') ? 'active' : '') }}">
                                         <a href="{{ url('admin/setting/display') }}"><i class="bi bi-chevron-right"></i> {{ trans('app.display_setting') }}</a>
                                     </li>
+                                    <li class="">
+                                        <a href="#"><i class="bi bi-chevron-right"></i> Advertisement</a>
+                                    </li>
                                     @endif
 
                                     <li class="{{ (Request::is('common/setting/*') ? 'active' : '') }}">
