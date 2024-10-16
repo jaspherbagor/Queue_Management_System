0a2cfab3b8c62975ffc1ad48d02449550ff2982e
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-10-16 18:56:20 +0800

    added a backipDatabase method in the DatabaseController

--- project/app/Http/Controllers/Admin/DatabaseController.php
+++ project/app/Http/Controllers/Admin/DatabaseController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Artisan;
 
 class DatabaseController extends Controller
 {
-    //
+    // Method to trigger the backup command
+    public function backupDatabase()
+    {
+        Artisan::call('backup:database');
+
+        return redirect()->back()->with('success', 'Database backup completed successfully.');
+    }
 }
