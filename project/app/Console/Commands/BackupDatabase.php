0f07d38bba91a079d002da45ebec3438a051dd7b
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-10-18 19:02:51 +0800

    added the carbon namespace

--- project/app/Console/Commands/BackupDatabase.php
+++ project/app/Console/Commands/BackupDatabase.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use Spatie\DbDumper\Databases\MySql;
+use Illuminate\Support\Carbon;
 
 class BackupDatabase extends Command
 {
@@ -39,14 +40,30 @@ class BackupDatabase extends Command
      */
     public function handle()
     {
-        File::put('dump.sql', '');
-        MySql::create()
-            ->setDbName(env('DB_DATABASE'))
-            ->setUserName(env('DB_USERNAME'))
-            ->setPassword(env('DB_PASSWORD'))
-            ->setHost(env('DB_HOST'))
-            ->setPort(env('DB_PORT'))
-            ->dumpToFile(base_path('dump.sql'));
+        // Get DB credentials from .env
+        $dbHost = env('DB_HOST');
+        $dbName = env('DB_DATABASE');
+        $dbUser = env('DB_USERNAME');
+        $dbPassword = env('DB_PASSWORD');
+
+        // Define the backup file name with timestamp
+        $fileName = 'backup-' . Carbon::now()->format('Y-m-d_H-i-s') . '.sql';
+        $filePath = storage_path('app/backups/' . $fileName);
+
+        // Command to back up the database using mysqldump
+        $command = "mysqldump --user={$dbUser} --password={$dbPassword} --host={$dbHost} {$dbName} > {$filePath}";
+
+        // Execute the command
+        $result = null;
+        $output = [];
+        exec($command, $output, $result);
+
+        // Check if backup was successful
+        if ($result === 0) {
+            $this->info('Database backup completed successfully.');
+        } else {
+            $this->error('Database backup failed.');
+        }
         
 
     }
