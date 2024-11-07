3a1e336a272adefb4dd167a646c8b3e9ffbddc55
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-11-07 18:09:23 +0800

    added a delete btn with its alert functionality in the backup files page

--- project/resources/views/backend/admin/setting/backup_file.blade.php
+++ project/resources/views/backend/admin/setting/backup_file.blade.php
@@ -17,7 +17,7 @@
                     <tr>
                         <th>Filename</th>
                         <th>Size (KB)</th>
-                        <th>Download</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -26,7 +26,8 @@
                             <td>{{ $file->getFilename() }}</td>
                             <td>{{ round($file->getSize() / 1024, 2) }}</td>
                             <td>
-                                <a href="{{ route('download_backup', $file->getFilename()) }}" class="btn btn-primary download-backup-btn">Download</a>
+                                <a href="{{ route('download_backup', $file->getFilename()) }}" class="btn btn-primary download-backup-btn mr-1">Download</a>
+                                <a href="#" class="btn btn-danger mb-1 delete-backup-btn">Delete</a>
                             </td>
                         </tr>
                     @empty
@@ -39,4 +40,30 @@
         </div>
     </div>
 </div>
+
+<script>
+    $(document).on('click', '.delete-backup-btn', function(e) {
+            e.preventDefault(); // Prevent the default action (navigation)
+
+            var url = $(this).attr('href'); // Get the URL from the href attribute
+
+            Swal.fire({
+                title: 'Are you sure you want to delete it?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire("Success!", "Deleted successfully", "success");
+                    // If confirmed, proceed with the deletion
+                    setTimeout(() => {
+                        window.location.href = url;
+                    }, 1500)
+                }
+            });
+        });
+</script>
 @endsection
